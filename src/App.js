bf5c4e8a1101370caeca56641dc84daa581178f2
Sakib <mmiazi@uncc.edu>
2020-04-07 18:46:14 -0400

    Pre-ALPHA

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends Component{
 
         this.state.isFetching = true;
 
-        fetch('http://107.152.37.166:5000/')
+        fetch('localhost:5000/')
             .then(results => {
                 // console.log(results.json())
                 this.json = results.json()
