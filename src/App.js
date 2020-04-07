1d82f0642721ebc3d1b7711673945918b9739c43
Sakib <mmiazi@uncc.edu>
2020-04-07 18:36:47 -0400

    Pre-ALPHA

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends Component{
 
         this.state.isFetching = true;
 
-        fetch('https://cors-anywhere.herokuapp.com/http://107.152.37.166:5000/')
+        fetch('http://107.152.37.166:5000/')
             .then(results => {
                 // console.log(results.json())
                 this.json = results.json()
